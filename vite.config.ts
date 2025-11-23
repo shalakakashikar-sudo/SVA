53228249f8c64792d2ca6ed0ec57ed3f52faa75c
shalakakashikar-sudo <shalakakashikar@gmail.com>
2025-11-23 16:46:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SVA/',    // <<--- add this
+  base: '/SVA/',
   plugins: [react()],
 })
